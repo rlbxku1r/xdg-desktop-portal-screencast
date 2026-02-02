9044920e9c74aa2cf82713af214c6462c9123e16
rlbxku1r <32811754+rlbxku1r@users.noreply.github.com>
2026-02-02 15:31:23 +0900

    sourceselector-ui: Use automatic type inference

--- crates/sourceselector-ui/src/main.rs
+++ crates/sourceselector-ui/src/main.rs
@@ -126,7 +126,7 @@ fn run(
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<_> = std::env::args().collect();
     if args.len() == 3 {
         let monitor_sources = &args[1];
         let window_sources = &args[2];
