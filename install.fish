01629a700b4ed06eaf2821831aebb33a484d6970
rlbxku1r <32811754+rlbxku1r@users.noreply.github.com>
2026-02-23 03:58:08 +0900

    install.fish: Use lowercase local variables

--- install.fish
+++ install.fish
@@ -3,9 +3,9 @@
 set SOURCE_DIR (status dirname)
 
 # Check for the prerequisite tools
-for TOOL in cargo rustc
-    if ! command -q $TOOL
-        echo "You must install the '$TOOL' to run this script." >&2
+for tool in cargo rustc
+    if ! command -q $tool
+        echo "You must install the '$tool' to run this script." >&2
         exit 1
     end
 end
@@ -17,9 +17,9 @@ function on_exit -e fish_exit
 end
 
 # Build these crates separately to avoid unnecessary library linking
-for CRATE in xdg-desktop-portal-screencast sourceselector-ui
-    if ! cargo b -r -p $CRATE --manifest-path $SOURCE_DIR/Cargo.toml --target-dir $TARGET_DIR
-        echo "Failed to build the '$CRATE' crate." >&2
+for crate in xdg-desktop-portal-screencast sourceselector-ui
+    if ! cargo b -r -p $crate --manifest-path $SOURCE_DIR/Cargo.toml --target-dir $TARGET_DIR
+        echo "Failed to build the '$crate' crate." >&2
         exit 1
     end
 end
